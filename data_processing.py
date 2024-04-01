1ff62758d260bca57a1f8acb553b637b3803b974
Rayhan Rizqi <rayhan.rizqi2@gmail.com>
2024-04-01 13:35:10 -0700

    Grouped Ethnicities and handled multi ethnic datapoints

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("CFAZ Modeling Data.csv")
+df = pd.read_csv("CforAZ_Data_Analytics\CFAZ Modeling Data.csv")
 
 #get rid of useless irrelevant weak ugly column "VANID"
 df.drop(columns=['VANID'], inplace=True)
@@ -31,7 +31,7 @@ df[['Gender', 'Ethnicity', 'Networth']] = df.apply(lambda row: pd.Series(extract
 df.drop(columns=['Bio'], inplace=True)
 
 # Display the modified DataFrame columns to verify the new structure
-print(df.columns)
+# print(df.columns)
 
 # Apply get_dummies to "Gender" and "Ethnicity" columns to create one-hot encoded columns
 gender_dummies = pd.get_dummies(df['Gender'], prefix='Gender')
@@ -41,28 +41,55 @@ ethnicity_dummies = pd.get_dummies(df['Ethnicity'], prefix='Ethnicity')
 df_with_dummies = pd.concat([df, gender_dummies], axis=1)
 df_with_dummies = pd.concat([df_with_dummies, ethnicity_dummies], axis=1)
 
+# Prettiy Gender columns
+for col in df_with_dummies.columns:
+    if "Gender_" in col:
+        newCol = col.split("_")
+        df_with_dummies = df_with_dummies.rename(columns={col: newCol[1]})
+
+# Prettify Ethnicity columns
 for col in df_with_dummies.columns:
     if "Ethnicity_" in col:
         newCol = col.split(" ")
-        print(newCol)
-        newCol = newCol[2]
-        df_with_dummies = df_with_dummies.rename(columns={col: newCol})
+        df_with_dummies = df_with_dummies.rename(columns={col: newCol[2]})
 
+
+ethnicColumns = ["afam", "chinese", "korean", "indian", "iranian", "japanese", "muslim", "white", "greek", "italian", "irish", "jewish", "latinx"]
+
+# very questionable variable name, might as well call it ethnic Cleansing table
 ethnicConversions = {"afam": "isAfam", "chinese": "isAsian", "korean": "isAsian",
                      "indian": "isAsian", "iranian": "isAsian", "japanese": "isAsian",
                      "muslim": "isAsian", "white": "isWhite", "greek": "isWhite",
                      "italian": "isWhite", "irish": "isIrish", "jewish": "isJewish",
                      "latinx": "isLatinx"}
 
+# for col in df_with_dummies.columns:
+#     if col in ethnicColumns:
+#         print(col + ": " + str(df_with_dummies[col].sum()))
+        
+#         df_with_dummies = df_with_dummies.
+#         ethnicConversions[col]
 
-for col in df_with_dummies.columns:
-    if ord(col[0]) >= 97:
-        print(col + ": " + str(df_with_dummies[col].sum()))
+# converting ethnicity columns to grouped ethnicity
+for original, new in ethnicConversions.items():
+    if new not in df_with_dummies.columns:
+        df_with_dummies[new] = 0
+    df_with_dummies[new] = df_with_dummies.apply(lambda row: 1 if row[original] == 1 or row[new] == 1 else 0, axis=1)
+    df_with_dummies = df_with_dummies.drop(columns={original})
 
-        ethnicConversions[col] =
+# handling multi ethnic people
+for col in df_with_dummies:
+    if "/" in col:
+        for eth in col.split("/"):
+            df_with_dummies[ethnicConversions[eth]] = 1
+        df_with_dummies = df_with_dummies.drop(columns={col})
 
-
+# Getting rid of initial Gender and Ethnicity columns
+df_with_dummies.drop(columns=['Gender'], inplace=True)
+df_with_dummies.drop(columns=['Ethnicity'], inplace=True)
 
 # Display the modified DataFrame columns to verify the new structure with dummies
 print(df_with_dummies.columns)
-print(df_with_dummies.head())
+# print(df_with_dummies.head())
+
+print(df_with_dummies["Total Contributions"])
