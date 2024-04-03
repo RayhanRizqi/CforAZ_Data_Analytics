5b2de05d30fbe80acbdefa103ea95b574d2b4d37
RayhanRizqi <rayhan.rizqi2@gmail.com>
2024-04-03 15:34:48 -0700

    Process Max Give

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,21 @@
 import pandas as pd
+import re
 
-df = pd.read_csv("CforAZ_Data_Analytics\CFAZ Modeling Data.csv")
+df = pd.read_csv("CFAZ Modeling Data.csv")
 
-#get rid of useless irrelevant weak ugly column "VANID"
+# get rid of useless irrelevant weak ugly column "VANID"
 df.drop(columns=['VANID'], inplace=True)
 df.drop(columns=['Employer'], inplace=True)
 df.drop(columns=['Occupation'], inplace=True)
 df.drop(columns=['City'], inplace=True)
 df.drop(columns=['State'], inplace=True)
+df.drop(columns=['Giving History'], inplace=True)
+
+# add columns about previous gives
+df['Max Give'] = 0
+df['Most Recent Give Time'] = 0
+df['Most Recent Give Amount'] = 0
+df['Number of Gives'] = 0
 
 # Adjust the process_string_corrected function to specifically extract Gender, Ethnicity, and Networth
 def extract_bio_details(bio_string):
@@ -89,7 +97,62 @@ df_with_dummies.drop(columns=['Gender'], inplace=True)
 df_with_dummies.drop(columns=['Ethnicity'], inplace=True)
 
 # Display the modified DataFrame columns to verify the new structure with dummies
-print(df_with_dummies.columns)
+# print(df_with_dummies.columns)
 # print(df_with_dummies.head())
 
-print(df_with_dummies["Total Contributions"])
+# 
+# print(df_with_dummies["Bio Contributions"])
+count = 0
+
+# Gets Max Give
+maxGive = 0
+for i in range(len(df_with_dummies)):
+    wordString = df_with_dummies.iloc[i]["Bio Contributions"].split("   ")
+
+    # handles if the row start with 
+    if (wordString[0] == "--------- all_contributions:"):
+        # removes the prefix "---- all contributions:"
+        donations = wordString[1:]
+        
+        # gets rid of leading & trailing white spaces & removes empty strings form lists, cuz for some reason they have empty strings
+        donations = [item for item in [item.strip() for item in donations] if item]
+
+        # get the list of all the numbers in the middle of the donation, then get the max of all of them
+        maxGive = int(max([float(s.split(' - ')[1]) for s in donations]))
+
+        # set the max Give in the df coluns "Max Give"
+        df_with_dummies.at[i, "Max Give"] = maxGive
+    else:
+        sections = ''.join(wordString)
+
+        # Split the sections by the "---------" delimiter
+        sections = [item for item in [item.strip() for item in sections.split("---------")] if item]
+        
+        # section = "Most recent ...", "Presidential", "Issue", "Most recent House"
+        for section in sections:
+            # Dont include the presidential donations
+            if "Presidential" in section:
+                continue
+
+            # Gets rid of the "Most recent ..." header and splits into list of donations in the section
+            section = [item for item in ''.join(section.split("\n")[1:]).split(", ") if item]
+            section = [item for item in section if item != ","]
+
+            # gets max dono amount
+            for donation in section:
+                # splits by empty strings and ","
+                pattern = r'[ ,]+'
+                donation = re.split(pattern, donation)
+
+                #removes empty strings
+                donation = [item for item in donation if item]
+
+                #if it includes donation amount
+                if len(donation) == 3:
+                    # Sometimes in place of the donation amount, its replaced by a uppercase character, so this takes it into account
+                    try:
+                        maxGive = max(maxGive, int(donation[len(donation)-1]))
+                    except ValueError:
+                        continue
+        # now that we have the max value, set it in the dataframe at column "Max Give"
+        df_with_dummies.at[i, "Max Give"] = maxGive
