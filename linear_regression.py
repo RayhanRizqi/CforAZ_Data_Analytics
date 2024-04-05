0e61daef47e3806fe7204bd79e4456b9605af8a9
Nivedh <nivedh@MacBook-Pro-3.local>
2024-04-05 11:51:12 -0700

    Added some epochs and removed bad data

--- linear_regression.py
+++ linear_regression.py
@@ -15,16 +15,25 @@ y = df['Total Contributions']  # Output
 # Splitting the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Creating and training the linear regression model
+# Creating the linear regression model
 model = LinearRegression()
-model.fit(X_train, y_train)
 
-# Predicting on the test set
-y_pred = model.predict(X_test)
+for epoch in range(100):
+    print("Epoch:", epoch + 1)
+    # Training the model
+    model.fit(X_train, y_train)
 
-# Evaluating the model
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+    # Predicting on the test set
+    y_pred = model.predict(X_test)
+    if epoch == 0 or epoch == 99:
+        print(y_pred)
+        for i in range(len(y_test)):
+            y_pred[i] = max(0,y_pred[i])
+            print("predicted: " + str(y_pred[i]) + " and expected: " + str(y_test.iloc[i]))
+
+    # Evaluating the model
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
 
 print("MSE: " + str(mse))
 print("r2: " + str(r2))
